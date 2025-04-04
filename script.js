ec79e8271b74c18389237938a85a9357bf29d503
sauravkumar11 <sauravsinghrajput1122002@gmail.com>
2025-04-04 20:16:44 +0530

    Remove JS Error

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function update() {
             dot.classList.remove('invisible');
         }
     });
- _
+
     hour.textContent = String(now.getHours()).padStart(2, '0');
     min.textContent = String(now.getMinutes()).padStart(2, '0');
     sec.textContent = String(now.getSeconds()).padStart(2, '0');
